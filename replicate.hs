b72fa1f3e001ea0173c7add8a898eca1c01a1d81
aidencullo <aidencullo@gmail.com>
2025-08-19 19:01:07 -0400

    Further updates to replicate.hs

--- replicate.hs
+++ replicate.hs
@@ -1,4 +1,5 @@
 replicate' :: (Num a, Ord a) => a -> b -> [b]
 replicate' n x
-  | n <= 0 = []
+  | n < 0 = error "cannot replicate something negative times"
+  | n == 0 = []
   | otherwise = x:replicate' (n - 1) x
