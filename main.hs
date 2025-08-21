c544e8e65699f2464e5321ca3094d816686d80d5
aidencullo <aidencullo@gmail.com>
2025-08-21 11:54:27 -0400

    Add main.hs that imports all modules

---
+++ main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import qualified Compare
+import qualified CompareWithHundred
+import qualified DivideByTen
+import qualified IsUpperCase
+import qualified Maximum
+import qualified Replicate
+import qualified Sign
+import qualified Take
+
+main :: IO ()
+main = return ()
