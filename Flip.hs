cc2af4ba590bc4e6f2922866d5fb0747c450230c
aidencullo <aidencullo@gmail.com>
2025-08-25 16:55:01 -0400

    Update Flip.hs and add cylinder.hs module

--- Flip.hs
+++ Flip.hs
@@ -1,4 +1,9 @@
 flip' :: (a -> b -> c) -> (b -> a -> c)
-flip' f =
-  let g x y = f y x
-  in g
+flip' f = let g x y = f y x in g
+
+useTest :: IO ()
+useTest = test (flip' (-) 10 5)
+
+test :: Int -> IO ()
+test (-5) = print "correct"
+test _    = print "wrong"
