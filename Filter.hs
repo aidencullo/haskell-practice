c7821097187bfefa2ef9592229b7b14bd684e81c
aidencullo <aidencullo@gmail.com>
2025-08-26 12:26:01 -0400

    Update Filter.hs

--- Filter.hs
+++ Filter.hs
@@ -4,15 +4,11 @@ filter' f (x:xs)
   | f x = x : filter' f xs
   | otherwise = filter' f xs
 
-greaterThanTwo :: Int -> Bool
-greaterThanTwo x = x > 2
-
 fives :: [Int]
 fives = replicate 5 5
 
 main :: IO ()
-main =
-  test (filter' greaterThanTwo fives)
+main = test (filter' (>2) fives)
 
 test :: [Int] -> IO ()
 test [5,5,5,5,5] = print "correct"
