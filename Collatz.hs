e97acc60d49da57b5d83539435f625396592d6e0
Aiden Cullo <aidencullo@gmail.com>
2025-09-10 18:37:01 -0400

    Add Collatz.hs

---
+++ Collatz.hs
@@ -0,0 +1,7 @@
+-- Now what we want to know is this: for all starting numbers between 1 and 100, how many chains have a length greater than 15? First off, we'll write a function that produces a chain:
+
+chain :: Int -> [Int]
+chain x
+      | x == 1 = [x]
+      | odd x = x : chain (3 * x + 1)
+      | even x = x : chain (2 * x)
