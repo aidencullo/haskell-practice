d205ec9801753710925748978536745643898ca1
Aiden Cullo <aidencullo@gmail.com>
2025-09-10 17:53:36 -0400

    Further update oddsSmallerThan.hs

--- oddsSmallerThan.hs
+++ oddsSmallerThan.hs
@@ -1,4 +1,4 @@
 -- find the sum of all odd squares that are smaller than 10,000
 
 sumOddsSmallerThan10000 :: Int
-sumOddsSmallerThan10000 = sum (filter odd [1..10000])
+sumOddsSmallerThan10000 = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
