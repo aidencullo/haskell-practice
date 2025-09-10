8dccace517c34820504593d24dc003c6c4a6abbd
Aiden Cullo <aidencullo@gmail.com>
2025-09-10 18:02:11 -0400

    Update oddsSmallerThan.hs with additional changes

--- oddsSmallerThan.hs
+++ oddsSmallerThan.hs
@@ -2,3 +2,8 @@
 
 sumOddsSmallerThan10000 :: Int
 sumOddsSmallerThan10000 = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
+
+-- using list comprehension
+
+sumOddsSmallerThan10000List :: Int
+sumOddsSmallerThan10000List = sum ( takeWhile (<10000) [ square | x <- [1..], let square = x ^ 2, odd square])
