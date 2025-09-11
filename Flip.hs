372d41086bacc8e811241b1fa80081427613a915
Aiden Cullo <aidencullo@gmail.com>
2025-09-11 17:35:35 -0400

    Update Flip.hs

--- Flip.hs
+++ Flip.hs
@@ -1,9 +1,2 @@
-flip' :: (a -> b -> c) -> (b -> a -> c)
-flip' f = let g x y = f y x in g
-
-useTest :: IO ()
-useTest = test (flip' (-) 10 5)
-
-test :: Int -> IO ()
-test (-5) = print "correct"
-test _    = print "wrong"
+flip' :: (a->b->c)->b->a->c
+flip' f x y = f y x
