3444613b855a0848d1b7fc2d0cc4762dce969ed9
Aiden Cullo <aidencullo@gmail.com>
2025-09-16 22:50:09 -0400

    Add comments and andTrue function to Infinite.hs

--- Infinite.hs
+++ Infinite.hs
@@ -2,5 +2,10 @@
 
 onethroughfour = take 4 (foldr (:) [] [1..])
 
+-- will return False (False && ..)
 andFalse :: Bool
 andFalse = foldr (&&) True (repeat False)
+
+-- inifinite
+andTrue :: Bool
+andTrue = foldr (&&) False (repeat True)
