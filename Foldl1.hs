2c691709c2296636db71a5721fd78c537d95f464
Aiden Cullo <aidencullo@gmail.com>
2025-09-22 17:26:14 -0400

    feat: Implement maximum' using foldl1
    This script now demonstrates how to implement the 'maximum' function using 'foldl1' as an example of the power of folds.

--- Foldl1.hs
+++ Foldl1.hs
@@ -1,4 +1,4 @@
 -- Just to show you how powerful folds are, we're going to implement a bunch of standard library functions by using folds:
 
-maximum' :: (Num a) => [a] -> a
+maximum' :: (Ord a) => [a] -> a
 maximum' xs = foldl1 max xs
