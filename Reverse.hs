027c7b432e12916b1968cf598f5d9e604739b0ea
Aiden Cullo <aidencullo@gmail.com>
2025-09-23 22:53:18 -0400

    auto-pushed changes

--- Reverse.hs
+++ Reverse.hs
@@ -1,7 +1,8 @@
--- reverse' = foldl (flip (:)) []
-
 reverse' :: [a] -> [a]
 reverse' = foldl (\acc x -> x : acc) []
 
 reverse1' :: [a] -> [a]
 reverse1' = foldr (\x acc -> acc ++ [x]) []
+
+reverse2' :: [a] -> [a]
+reverse2' = foldl (flip (:)) []
