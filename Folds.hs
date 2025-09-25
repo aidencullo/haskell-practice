300f4fe911d1e77614c26ac21ad31806330de285
Aiden Cullo <aidencullo@gmail.com>
2025-09-25 15:29:54 -0400

    implement product via general foldl and foldr

--- Folds.hs
+++ Folds.hs
@@ -1,10 +1,22 @@
 product' :: (Num a) => [a] -> a
--- product' = foldr1 (\x acc -> acc * x)
 product' = foldr1 (*)
 
 product2' :: (Num a) => [a] -> a
 product2' = foldl1 (*)
 
-oneToTen = [1..10]
-testProducts = product' oneToTen == product2' oneToTen && product' oneToTen == product oneToTen
+product3' :: (Num a) => [a] -> a
+product3' = foldl (*) 1
 
+product4' :: (Num a) => [a] -> a
+product4' = foldr (*) 1
+
+oneToTen :: [Int]
+oneToTen = [1..10]
+
+testProducts :: Bool
+testProducts =
+    let expected = product oneToTen
+    in  product' oneToTen  == expected
+     && product2' oneToTen == expected
+     && product3' oneToTen == expected
+     && product4' oneToTen == expected
