c61485dde020b5c2790c91e4430f53e4e01018c5
Aiden Cullo <aidencullo@gmail.com>
2025-09-25 15:27:46 -0400

    implement product via foldl1

--- Folds.hs
+++ Folds.hs
@@ -1,3 +1,10 @@
 product' :: (Num a) => [a] -> a
 -- product' = foldr1 (\x acc -> acc * x)
 product' = foldr1 (*)
+
+product2' :: (Num a) => [a] -> a
+product2' = foldl1 (*)
+
+oneToTen = [1..10]
+testProducts = product' oneToTen == product2' oneToTen && product' oneToTen == product oneToTen
+
