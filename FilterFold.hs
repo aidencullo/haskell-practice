22bd656f4069f1475f1846de9cfaebc521e8eba8
Aiden Cullo <aidencullo@gmail.com>
2025-09-30 14:14:19 -0400

    filter using foldl

--- FilterFold.hs
+++ FilterFold.hs
@@ -1,3 +1,9 @@
+filterLeft :: (a -> Bool) -> [a] -> [a]
+filterLeft f xs = foldl (\acc x -> if f x then acc ++ [x] else acc) [] xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f xs = foldr (\x acc -> if f x then x : acc else acc) [] xs
+
 filterAndReverse :: (a -> Bool) -> [a] -> [a]
 filterAndReverse f xs = foldl (\acc x -> if f x then x : acc else acc) [] xs
 
@@ -9,8 +15,13 @@ testFilterAndReverse =
   let expected = reverse (filter even oneToTen)
   in filterAndReverse even oneToTen == expected
 
--- testFilter :: Bool
--- testFilter =
---   let expected = filter even oneToTen
---   in filter' even oneToTen == expected
+testFilterLeft :: Bool
+testFilterLeft =
+  let expected = filter even oneToTen
+  in filterLeft even oneToTen == expected
+
+testFilter :: Bool
+testFilter =
+  let expected = filter even oneToTen
+  in filter' even oneToTen == expected
   
