ed89fc7c8471265637dd2c10244805fdb16d004f
Aiden Cullo <aidencullo@gmail.com>
2025-09-30 14:45:24 -0400

    implement last using fold

---
+++ LastFold.hs
@@ -0,0 +1,15 @@
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+ten :: Int
+ten = 10
+
+oneToTen :: [Int]
+oneToTen = [1..10]
+
+testLast :: Bool
+testLast = let expected = ten
+  in expected == last' oneToTen
+
+test :: Bool
+test = testLast
