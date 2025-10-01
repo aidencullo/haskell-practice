601f319d91684b252e7d5003f96b3c01d0d91cd2
Aiden Cullo <aidencullo@gmail.com>
2025-10-01 18:19:06 -0400

    implement flip and .

--- HigherOrderFunctions.hs
+++ HigherOrderFunctions.hs
@@ -2,3 +2,14 @@
 
 sumSqrt :: Int
 sumSqrt = length(takeWhile (<1000) (scanl1 (+) (map (sqrt) [1..]))) + 1
+
+-- flip' f = \x y -> f y x
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = 
+  let g x y = f y x
+  in g
+  
+(.:) :: (c -> a) -> (b -> c) -> (b -> a)
+(.:) f g = \x -> f $ g x
+
+
