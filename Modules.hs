c52befb22a214b9a91e6940f49518a7e72f16919
Aiden Cullo <aidencullo@gmail.com>
2025-10-02 17:46:18 -0400

    demonstrate namespace clash

--- Modules.hs
+++ Modules.hs
@@ -1,18 +1,19 @@
-import Data.List  
+import Data.List
+-- this will cause a clash error on filter
+-- import Data.Map
 
 -- create a sample datatype
 data MyBool = MyTrue | MyFalse
 
-
--- and typeclass instance
+-- typeclass instance
 instance Eq MyBool where
     MyTrue  == MyTrue  = True
     MyFalse == MyFalse = True
     _       == _       = False
 
-
--- this will cause a parse error
--- import Data.List  
-
+-- using Data.List
 numUniques :: (Eq a) => [a] -> Int  
 numUniques = length . nub  
+
+
+filtered = filter (\v -> v == 'a') ['a'..'z']
