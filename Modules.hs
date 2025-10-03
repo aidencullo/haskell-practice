e15bb4dc40f1ae9ed0ad42d38aa3afeb0597dc8b
Aiden Cullo <aidencullo@gmail.com>
2025-10-03 14:25:04 -0400

    formatting/linting

--- Modules.hs
+++ Modules.hs
@@ -1,61 +1,48 @@
-import Data.List
--- this will cause a clash error on filter
--- import Data.Map
-import qualified Data.Map  
+import Data.List (nub)
+import qualified Data.Map as Map
 
--- create a sample datatype
+-- Sample datatype
 data MyBool = MyTrue | MyFalse
 
--- typeclass instance
+-- Typeclass instance
 instance Eq MyBool where
     MyTrue  == MyTrue  = True
     MyFalse == MyFalse = True
     _       == _       = False
 
--- using Data.List
-numUniques :: (Eq a) => [a] -> Int  
-numUniques = length . nub  
+-- Using Data.List
+numUniques :: Eq a => [a] -> Int
+numUniques = length . nub
 
+-- Using Data.Map
+myMap :: Map.Map Int Char
+myMap = Map.fromList [(1,'a'), (2,'b')]
 
-myMap = Data.Map.fromList [(1,'a'), (2,'b')]
-filtered = Data.Map.filter (\v -> v == 'a') myMap
+filtered :: Map.Map Int Char
+filtered = Map.filter (== 'a') myMap
 
-
--- Define a type
+-- Shape datatype
 data Shape = Circle Float | Rectangle Float Float
 
--- Circle and Rectangle are *constructors*
--- They create values of type Shape
 c1 :: Shape
-c1 = Circle 3.0   -- uses the Circle constructor
+c1 = Circle 3.0
 
 r1 :: Shape
-r1 = Rectangle 4.0 5.0  -- uses the Rectangle constructor
+r1 = Rectangle 4.0 5.0
 
--- implement allSame
-allSame :: (Eq a) => [a] -> Bool
-allSame = (==1) . (length . nub)
+-- allSame function
+allSame :: Eq a => [a] -> Bool
+allSame xs = length (nub xs) == 1
 
--- -- implement allSame
--- allSame :: (Eq a) => [a] -> Bool
--- allSame xs = all (== head xs) xs
-
--- test allSame
-testAllSame1 :: Bool
-testAllSame1 = allSame (replicate 10 10) == True
-
-testAllSame2 :: Bool
-testAllSame2 = allSame (replicate 10 100) == True
-
-testAllSame3 :: Bool
+-- Tests
+testAllSame1, testAllSame2, testAllSame3, testAllSame4 :: Bool
+testAllSame1 = allSame (replicate 10 10)
+testAllSame2 = allSame (replicate 10 100)
 testAllSame3 = allSame [1..10] == False
-
-testAllSame4 :: Bool
-testAllSame4 = allSame (take 100 $ repeat 10) == True
+testAllSame4 = allSame (take 100 $ repeat 10)
 
 testAllSames :: [Bool]
 testAllSames = [testAllSame1, testAllSame2]
 
-testAllSame :: Bool
-testAllSame = and testAllSames
-
+testAllSameOverall :: Bool
+testAllSameOverall = and testAllSames
