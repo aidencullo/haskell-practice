1f62321b9a4c4be8a718a7885ac0820e4b11e53c
Aiden Cullo <aidencullo@gmail.com>
2025-10-03 14:08:32 -0400

    implement allSAme

--- Modules.hs
+++ Modules.hs
@@ -31,3 +31,18 @@ c1 = Circle 3.0   -- uses the Circle constructor
 
 r1 :: Shape
 r1 = Rectangle 4.0 5.0  -- uses the Rectangle constructor
+
+-- implement allSame
+allSame :: (Eq a) => [a] -> Bool
+allSame xs = (length . nub) xs == 1
+
+testAllSame1 :: Bool
+testAllSame1 = allSame (replicate 10 10) == True
+
+testAllSame2 :: Bool
+testAllSame2 = allSame (replicate 10 100) == True
+
+testAllSame :: Bool
+testAllSame = testAllSame1
+  && testAllSame2
+
